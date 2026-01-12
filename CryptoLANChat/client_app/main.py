8b39a1070b26084ba7a7ca4494d748fca8babc52
meto820 <metekurubal12@gmail.com>
2026-01-12 13:34:48 +0300

    Delete CryptoLANChat directory

--- CryptoLANChat/client_app/main.py
+++
@@ -1,8 +0,0 @@
-import sys
-from PySide6.QtWidgets import QApplication
-from client_app.ui.login import LoginWindow
-
-app = QApplication(sys.argv)
-win = LoginWindow()
-win.show()
-sys.exit(app.exec())
--- CryptoLANChat/share/config.py
+++
@@ -1,7 +0,0 @@
-# share/config.py
-
-HOST = "0.0.0.0"
-PORT = 9000
-
-# 32 byte AES anahtarı
-ROOM_KEY = b"0123456789abcdef0123456789abcdef"
